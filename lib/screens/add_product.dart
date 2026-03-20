4013c2670d0177f1174b40914bd5fe1520a542db
Leonel Eisinger <42424255+leonelEisinger@users.noreply.github.com>
2026-03-20 20:39:16 -0300

    Update add_product.dart

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -5,10 +5,13 @@ class addNew extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
       return Scaffold(
-        appBar: AppBar(title: Text("Add new product to list")),
+        appBar: AppBar(title: Text("Add new Task to list")),
         body: Column(
           children: [
-            Text('Worked fine'),
+            Title(
+              color: Colors.black, 
+              child: Text('Type the name and qtd of people'),
+              )
           ],
         ),
       );

72f22c7050572551e4b22377fc755d74a32a64a8
Leonel Eisinger <42424255+leonelEisinger@users.noreply.github.com>
2026-03-20 21:36:05 -0300

    reupload with new code

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:a3/widgets/task_item.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/task_provider.dart';
+import 'package:flutter/material.dart';
+import '../widgets/task_list.dart';
+import '../screens/add_product.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final taskProvider = Provider.of<TaskProvider>(context);
+
+    var idle = 0; // Total Comprados <- if comprado/marcado ou nesse caso total de pessoas que terminaram a tarefa -> +
+    var tasks = taskProvider.totalTasks(); // para Comprar
+    var total = 0; // qtd total de produtos
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Assigned People To Tasks")),
+      body: Column(
+        children: [
+          Text('Idle people: $idle'),
+          Text('Total tasks: $tasks'),
+          Text('Total people: $total'),
+
+          TaskList(),
+          
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(
+            builder: (context) => addNew()
+            )
+          )
+        },
+        child: const Icon(Icons.add)
+      ),
+    );
+  }
+}
